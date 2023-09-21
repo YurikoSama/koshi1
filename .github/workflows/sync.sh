47a40283cd64eca247f1fc1255d2c3ef15436b55
YurikoSama <1142430166@qq.com>
2023-09-21 21:07:29 +0800

    Initialize develop

---
+++ .github/workflows/sync.sh
@@ -0,0 +1,23 @@
+mkdir -p $RUNNER_TEMP/dist
+cp -a * $RUNNER_TEMP/dist
+cp -a .github $RUNNER_TEMP/dist
+cp .* $RUNNER_TEMP/dist
+mv $RUNNER_TEMP/dist dist
+
+cd dist
+
+# modify package.json
+cat package.json | jq '.version="0.0.0" | del(.optionalDependencies)' > package.json.tmp
+mv -f package.json.tmp package.json
+
+# modify workflows
+cd .github/workflows
+rm publish.sh
+rm sync.sh
+rm sync.yml
+
+sed -i tag.yml \
+-e 's/develop/master/g'
+
+sed -i build.yml \
+-e 's/develop/master/g'
